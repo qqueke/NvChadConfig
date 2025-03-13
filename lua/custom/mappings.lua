6b30e995d1ac89be3b3589f038ec2b97e7f8a6d0
QQueke <QQueker@gmail.com>
2025-03-13 11:58:42 +0000

    Auto-sync nvim config

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -8,7 +8,8 @@ M.tmux = {
     ["<C-l>"] = { "<cmd> TmuxNavigateRight<CR>", "window right" },
     ["n"] = { "nzz", "Center search result" },
     ["N"] = { "Nzz", "Center search result" },
-
+    ["j"] = { "jzz", "Move down and center" },
+    ["k"] = { "kzz", "Move up and center" },
     ["<Enter>"] = { "o<ESC>zz", "Center search result" },
   },
 }
