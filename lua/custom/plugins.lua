8d0b571b00477f8b889304b8e55a142c02c24685
QQueke <QQueker@gmail.com>
2025-04-06 07:46:55 +0100

    Auto-sync nvim config

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -2,13 +2,22 @@ plugins = {
   {
     "folke/noice.nvim",
     event = "VeryLazy",
-    opts = {},
+    -- opts = {},
     dependencies = {
       "MunifTanjim/nui.nvim",
       "rcarriga/nvim-notify",
     },
-    config = {
+    keys = {
+      { "<leader>dm", "<cmd>Noice dismiss<CR>", desc = "Dismiss notifications" },
+    },
+    opts = {
       lsp = {
+        -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+        override = {
+          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+          ["vim.lsp.util.stylize_markdown"] = true,
+          ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+        },
         signature = {
           enabled = false,
         },
@@ -16,7 +25,24 @@ plugins = {
           enabled = false,
         },
       },
+      presets = {
+        bottom_search = true, -- use a classic bottom cmdline for search
+        command_palette = true, -- position the cmdline and popupmenu together
+        long_message_to_split = true, -- long messages will be sent to a split
+        inc_rename = false, -- enables an input dialog for inc-rename.nvim
+        lsp_doc_border = false, -- add a border to hover docs and signature help
+      },
     },
+    -- config = {
+    --   lsp = {
+    --     signature = {
+    --       enabled = false,
+    --     },
+    --     hover = {
+    --       enabled = false,
+    --     },
+    --   },
+    -- },
   },
   {
     "kristijanhusak/vim-dadbod-ui",
